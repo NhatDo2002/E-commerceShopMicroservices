4f806aa66be24ec1a2a3b6410c8ac328b10732ee
Đỗ Minh Nhật <87433277+NhatDo2002@users.noreply.github.com>
2025-12-19 07:35:17 +0700

    Implement Entity, Domain Event, Aggerate abstractions in Ordering service

---
+++ source/Services/Ordering/Ordering.Domain/Abstractions/Entity.cs
@@ -0,0 +1,11 @@
+﻿namespace Ordering.Domain.Abstractions
+{
+    public abstract class Entity<T> : IEntity<T>
+    {
+        public T Id { get; set; }
+        public string? CreateBy { get; set; }
+        public DateTime? CreateDate { get; set; }
+        public string? ModifyBy { get; set; }
+        public DateTime? ModifyDate { get; set; }
+    }
+}
---
+++ source/Services/Ordering/Ordering.Domain/Abstractions/IEntity.cs
@@ -0,0 +1,15 @@
+﻿namespace Ordering.Domain.Abstractions
+{
+    public interface IEntity<T> : IEntity
+    {
+        public T Id { get; set; }
+    }
+
+    public interface IEntity
+    {
+        public string? CreateBy { get; set; }
+        public DateTime? CreateDate { get; set; }
+        public string? ModifyBy { get; set; }
+        public DateTime? ModifyDate { get; set; }
+    }
+}
