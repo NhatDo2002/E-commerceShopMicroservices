a38e818ad202619cfcc49a621438b2687945ec82
NhatDo2002 <nhatdo0511>
2026-01-14 11:41:02 +0700

    Implement models in webapp for interacting with services

---
+++ source/WebApps/Shopping.Web/Models/Basket/ShoppingCartModel.cs
@@ -0,0 +1,24 @@
+﻿namespace Shopping.Web.Models.Basket
+{
+    public class ShoppingCartModel
+    {
+        public string UserName { get; set; } = default!;
+        public List<ShoppingCartItemModel> Items { get; set; } = new List<ShoppingCartItemModel>();
+        public decimal ToTalPrice => Items.Sum(x => x.Price * x.Quantity);
+    }
+
+    public class ShoppingCartItemModel
+    {
+        public int Quantity { get; set; }
+        public string? Color { get; set; }
+        public decimal Price { get; set; }
+        public Guid ProductId { get; set; }
+        public string? ProductName { get; set; }
+    }
+
+    //Wrapper classes
+    public record GetBasketResponse(ShoppingCartModel Cart);
+    public record StoreBasketRequest(ShoppingCartModel Cart);
+    public record StoreBasketResponse(string Username);
+    public record DeleteBasketResponse(bool IsSuccess);
+}
---
+++ source/WebApps/Shopping.Web/Models/Ordering/OrderModel.cs
@@ -0,0 +1,38 @@
+﻿namespace Shopping.Web.Models.Ordering
+{
+    public record OrderModel
+    (
+        Guid Id,
+        Guid CustomerId,
+        string OrderName,
+        AddressModel ShippingAddress,
+        AddressModel BillingAddress,
+        PaymentModel Payment,
+        OrderStatus Status,
+        decimal TotalAmount,
+        List<OrderItemModel> OrderItems
+    );
+
+    public record AddressModel(string FirstName, string LastName, string? EmailAddress, string AddressLine, string Country, string State, string ZipCode);
+    public record PaymentModel(string? CardName, string CardNumber, string Expiration, string Cvv, int PaymentMethod);
+    public record OrderItemModel(Guid OrderId, Guid ProductId, int Quantity, decimal Price);
+    public enum OrderStatus
+    {
+        Draft = 1,
+        Pending = 2,
+        Completed = 3,
+        Cancelled = 4
+    }
+
+    //Wrapper classes
+    //Get orders
+    public record GetOrdersResponse(PaginatedResult<OrderModel> Orders);
+    public record GetOrdersByCustomerIdResponse(IEnumerable<OrderModel> Orders);
+    public record GetOrdersByNameResponse(IEnumerable<OrderModel> Orders);
+    //Delete order
+    public record DeleteOrderResponse(bool IsSuccess);
+    //Create order
+    public record CreateOrderRequest(OrderModel OrderRequest);
+    public record CreateOrderResponse(Guid OrderId);
+
+}
